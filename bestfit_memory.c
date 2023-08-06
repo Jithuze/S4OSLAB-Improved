e9a2177ebba69c17d121c1666dc74c6c9a174fb1
Jithuze <125732084+Jithuze@users.noreply.github.com>
2023-08-06 20:57:59 +0530

    Create bestfit_memory.c

---
+++ bestfit_memory.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,block_no,block_size[10],process_no,process_size[10];
+    
+    printf("Enter no of free blocks :\t");
+    scanf("%d",&block_no);
+    
+    printf("Enter the size of free blocks :\n");
+    for(i=0;i<block_no;i++){
+        scanf("%d",&block_size[i]);
+    }
+    
+    printf("Enter no of processes :\t");
+    scanf("%d",&process_no);
+    
+    printf("Enter size of processes : \n");
+    for(i=0;i<process_no;i++){
+        scanf("%d",&process_size[i]);
+    }
+    
+    printf("\tFIRST FIT\n");
+    
+    for(i=0;i<block_no;i++){
+        printf("size of free block %d: %d\n",i+1,block_size[i]);
+    }
+    printf("\n\n");
+    for(i=0;i<process_no;i++){
+        printf("size of process %d: %d\n",i+1,process_size[i]);
+    }
+    printf("\n");
+  
+    printf("PNo\tAllocBlock\tAllocSize\tFragInBlock\n");
+    
+    i=0;
+    int small = block_size[0];
+    while(i<process_no){
+        for(j=0;j<block_no;j++){
+            if(process_size[i]<=block_size[j]){  //if process size is less then block size ,allocate it 
+                small = block_size[j];
+            }
+        }
+        for(j=0;j<block_no;j++){
+            if(block_size[j]==small){
+                block_size[j] -= process_size[i];
+                break;
+            }
+        }
+        
+        printf("%d\t%d\t\t%d\t\t%d\n",i+1,j+1,process_size[i],block_size[j]);
+        i++;
+    }
+    return 0;
+}
